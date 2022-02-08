88eade1cb172b39f348f589768e98860846e1173
forrestmorrison <forrestmorrison@gmail.com>
2022-02-08 08:32:51 -0600

    updated Address Book

--- dom-addressBook.js
+++ dom-addressBook.js
@@ -1,14 +1,23 @@
-let userArray = [];
-
+let addressBook = [];
 
 const fetchUser = () => {
     fetch("https://randomuser.me/api")
     .then(response => response.json())
-    .then(json => userArray.push(json))
+    .then(json => addressBook.push(json))
     .catch(error => console.log(error))
 }
 
 const insideArray = () => {
-    console.log(userArray[0].results[0].name);
+    console.log(addressBook);
 }
 
+const listUsers = (arr) => {
+    let userList = document.getElementById("user-list");
+    for (let i=0; i < arr.length; i++) {
+        let newListItem = document.createElement('LI');
+        newListItem.innerText = arr[0].results[0].name;
+        userList.appendChild(newListItem);
+    }
+}
+
+// console.log(userArray[0].results[0].name);
